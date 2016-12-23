4e19845f7f9e08e16b5e8a15e2d75d423df0142f
Elliot Plant <elliotaplant@gmail.com>
2016-12-22 17:23:45 -0800

    Checkpoint

--- server/chreddit-server.js
+++ server/chreddit-server.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var app = express()
+var http = require('http');
 
-app.get('/', function (req, res) {
+app.get('/frontpage', function (req, res) {
+  var options = {
+    host: 'www.random.org',
+    path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+  };
+  http.request(options, handleRedditResponse).end();
+
   res.send('Hello World!')
 })
 
@@ -10,3 +17,20 @@ app.listen(3000, function () {
 })
 
 app.use(express.static('client'))
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+
+
+var handleRedditResponse = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log("ending response:", str);
+  });
+}
