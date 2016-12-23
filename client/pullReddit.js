4a1e119f8ba84bc2e592ac445243fc8144d1e0d8
Elliot Plant <elliotaplant@gmail.com>
2016-12-22 17:43:51 -0800

    Try http

--- client/pullReddit.js
+++ client/pullReddit.js
@@ -1,16 +1,7 @@
 function pullReddit() {
     console.log("pulling reddit!")
-    $.ajax({
-        url: "https://www.reddit.com",
-        type: "GET",
-        crossDomain: true,
-        success: function(response) {
-            console.log("response:", response)
-            // var resp = JSON.parse(response)
-        },
-        error: function(xhr, status) {
-            console.log("error", xhr, status);
-        }
+    $.get("frontpage", function(response) {
+      console.log("got it back!", response)
     });
 }
 // Shorthand for $( document ).ready()
--- server/chreddit-server.js
+++ server/chreddit-server.js
@@ -1,18 +1,17 @@
 var express = require('express')
 var app = express()
 var http = require('http');
+// var request = require('request');
 
-app.get('/frontpage', function (req, res) {
+app.get('/frontpage', function(req, res) {
   var options = {
-    host: 'www.random.org',
-    path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+    host: 'www.reddit.com',
+    // path: ''
   };
-  http.request(options, handleRedditResponse).end();
-
-  res.send('Hello World!')
+  http.request(options, handleRedditResponse(res)).end();
 })
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 })
 
@@ -20,17 +19,30 @@ app.use(express.static('client'))
 
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 
+var handleRedditResponse = function handleRedditResponse(res) {
 
-var handleRedditResponse = function(response) {
-  var str = '';
+  return function(response) {
+    var str = '';
 
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function(chunk) {
+      str += chunk;
+      console.log("got chunk:", chunk);
+    });
 
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log("ending response:", str);
-  });
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function() {
+      console.log("ending response:", str);
+      res.send("Response:" + str)
+    });
+
+  }
 }
+
+
+// request.get('http://www.whatever.com/my.csv', function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         var csv = body;
+//         // Continue with your processing here.
+//     }
+// });
