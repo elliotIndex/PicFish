c215a8b342a4b4cc863feceb991c1664b8702573
Elliot Plant <elliotaplant@gmail.com>
2016-12-25 15:50:31 -0800

    All imgs on the front

--- client/pullReddit.js
+++ client/pullReddit.js
@@ -1,11 +1,26 @@
 function pullReddit() {
-    console.log("pulling reddit!")
-    $.get("frontpage", function(response) {
-      console.log("got it back!", response)
-    });
+  console.log('Pulling reddit!')
+  $.get('frontpage', function(response) {
+    console.log('Building list!', response)
+    buildRedditList(response);
+  });
 }
 // Shorthand for $( document ).ready()
 $(function() {
-    console.log("ready!");
-    pullReddit();
+  console.log('ready!');
+  pullReddit();
 });
+
+function buildRedditList(links) {
+  var imageList = $('#image-list');
+  links.forEach(function (link) {
+    var listItem = $('<li>');
+    var anchor = $('<a>', { href: link.href, text: link.text });
+    console.log('Link:', link.href);
+    var image = $('<img>', { src: link.href, class: 'link-img' });
+    listItem.append(anchor);
+    listItem.append(image);
+    imageList.append(listItem);
+  });
+  console.log('Built list');
+}
--- server/chreddit-server.js
+++ server/chreddit-server.js
@@ -1,6 +1,6 @@
-var express = require('express')
-var app = express()
-
+var express = require('express');
+var app = express();
+var request = require('request');
 var parseFrontPage = require('./parseFrontPage');
 
 var redditLinks = [];
@@ -8,28 +8,43 @@ var redditLinks = [];
 console.log('Fetching')
 parseFrontPage(function (err, window) {
   console.log('Parsing');
+  var allLinks = [];
   var $ = window.$;
   var links = $('.title.may-blank');
-  links.each(function() {
+  links.each(function () {
     var element = $(this);
-    redditLinks.push({
+    allLinks.push({
       text: element.text(),
       href: element.attr('href')
     })
   });
-  console.log('Filtering');
-  redditLinks = redditLinks.filter(function (link) {
-    return link.href.indexOf('imgur') > -1;
+  console.log('Mapping');
+  allLinks = allLinks.map(function (link) {
+    if (link.href.indexOf('imgur') > -1 &&
+      !(link.href.endsWith('.jpg') ||
+      link.href.endsWith('.png') ||
+      link.href.endsWith('.gif')) ) {
+        link.href = link.href + '.jpg';
+    }
+    return link;
   });
-  console.log('Ready!');
+  allLinks.forEach(function (link) {
+    request(link.href, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        redditLinks.push(link);
+        console.log('pushed a link:', link.href);
+      }
+    });
+  });
 });
 
 
-app.get('/frontpage', function(req, res) {
+app.get('/frontpage', function (req, res) {
+  console.log('serving request');
   res.send(redditLinks);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
 
