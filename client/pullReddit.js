47c065773bdf0be6208ed857d1662c6178f3e6cf
Elliot Plant <elliotaplant@gmail.com>
2016-12-26 15:17:02 -0800

    Put share link on page

--- client/pullReddit.js
+++ client/pullReddit.js
@@ -19,8 +19,10 @@ function buildRedditList(links) {
 function buildListItem(link) {
   var listItem = $('<li>', { class: 'list-entry' });
   var anchor = $('<h3>', { text: link.text, class: 'link-title' });
+  var share = $('<h5>', { text: buildShareLink(link), class: 'link-share' });
   var image = $('<img>', { src: link.href, class: 'link-img' });
   listItem.append(anchor);
+  listItem.append(share);
   listItem.append(image);
   return listItem;
 }
