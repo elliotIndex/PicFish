a003d7a6548c6dd245eae176832ebddf1fb7c529
Elliot Plant <elliotaplant@gmail.com>
2016-12-26 14:06:49 -0800

    built list

--- client/pullReddit.js
+++ client/pullReddit.js
@@ -1,30 +1,26 @@
 function pullReddit() {
   console.log('Pulling reddit!')
   $.get('frontpage', function(response) {
-    console.log('Building list!', response)
     buildRedditList(response);
   });
 }
+
 // Shorthand for $( document ).ready()
-$(function() {
-  console.log('ready!');
-  pullReddit();
-});
+$(function() { pullReddit(); });
 
 function buildRedditList(links) {
   var imageList = $('#image-list');
   links.forEach(function (link) {
     imageList.append(buildListItem(link));
   });
+  console.log('Built list');
 }
 
 function buildListItem(link) {
-  var listItem = $('<li>'); // , { 'class': 'list-group-item'});
+  var listItem = $('<li>');
   var anchor = $('<h3>', { text: link.text, class: 'link-title' });
   var image = $('<img>', { src: link.href, class: 'link-img' });
   listItem.append(anchor);
   listItem.append(image);
   return listItem;
 }
-
-console.log('Built list');
