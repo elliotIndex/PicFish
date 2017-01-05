07d24bd41769acd85fbaa91373bd935c2034bbf8
Elliot Plant <elliotaplant@gmail.com>
2017-01-04 21:08:36 -0800

    Remove logs

--- server/chreddit-server.js
+++ server/chreddit-server.js
@@ -14,17 +14,14 @@ parseFrontPage(globalStorageContext);
 app.use(express.static('../client'));
 
 app.get('/frontpage', function (req, res) {
-  console.log('Full page');
   res.send(globalStorageContext.redditLinks);
 });
 
 app.get('/:id', function (req, res) {
-  console.log('Sharing page');
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
 
 app.get('/picture/:id', function (req, res) {
-  console.log('Sharing picture');
   const defaultLink = {
     text: 'Uh oh! We couldn\'t find the picture you were looking for. We\'ll try to fix it on our end :)',
     href: 'http://imgur.com/PbcZq8t.jpg',
