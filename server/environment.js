c4768edf0519333d4fdbac35611380687092d89b
Elliot Plant <elliotaplant@gmail.com>
2017-01-09 00:30:40 -0800

    Change mongo url

--- server/environment.js
+++ server/environment.js
@@ -1,12 +1,14 @@
 devEnvironment = {
   mode: "dev",
   domain: "localhost:3000",
+  mongoUrl: 'mongodb://localhost:27017/linksDb',
   port: 3000,
 }
 
 prodEnvironment = {
   mode: "prod",
   domain: "www.pic.fish",
+  mongoUrl: 'mongodb://0.0.0.0:27017/linksDb',
   port: 3000,
 }
 
