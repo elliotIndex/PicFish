8250f54ca26694ee15c669e5b2d4f1b09eb95465
Elliot Plant <elliotaplant@gmail.com>
2017-01-09 02:10:32 -0800

    Add

--- server/chreddit-server.js
+++ server/chreddit-server.js
@@ -8,10 +8,6 @@ const environment = require('./environment');
 const fileServer = require('./fileServer');
 const utils = require('./utils');
 
-// app.get('/style.css', function (req, res) {
-//   fileServer.sendStyleSheet(req, res);
-// });
-
 app.get('/', function (req, res) {
   database.incrementVisitCount();
   fileServer.serveDefaultFile(req, res);
