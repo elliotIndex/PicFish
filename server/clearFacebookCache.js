39ddaa5c8658268a330af6bbb05494f7d1928865
Elliot Plant <elliotaplant@gmail.com>
2017-01-12 01:27:00 -0800

    Add clearing to facebook cache

---
+++ server/clearFacebookCache.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+module.exports = (terminal) => {
+  request.post(
+    'https://graph.facebook.com',
+    { id: 'http://www.pic.fish/terminal', scrape: true },
+    () => {}
+  );
+};
--- server/scheduleLinkRefresh.js
+++ server/scheduleLinkRefresh.js
@@ -2,6 +2,7 @@ const cron = require('node-cron');
 const fetchSubredditLinks = require('./fetchSubredditLinks');
 const globals = require('./globals');
 const database = require('./database');
+const clearFacebookCache = require('./clearFacebookCache');
 const utils = require('./utils');
 const renderTemplate = require('./rendering/renderTemplate');
 
@@ -16,36 +17,37 @@ function fetchAllSubreddits(subreddits) {
       globals.renderedSubredditsDir,
       utils.toTitleCase(picFishSub)
     ))
-    .catch(error => console.error("Error rendering subreddit:", subreddit, error));
+    .catch(error => console.error("Error rendering subreddit:", subreddit, error))
+    .then(() => clearFacebookCache(picFishSub));
 
-    subredditLinks
-    .then(links => database.insertLinks(links))
-    .catch(error => console.error("Error storing subreddit:", subreddit, error));
+      subredditLinks
+      .then(links => database.insertLinks(links))
+      .catch(error => console.error("Error storing subreddit:", subreddit, error));
+    }
   }
-}
 
-function scheduleLinkRefresh(subreddits) {
-  renderDevPage();
+  function scheduleLinkRefresh(subreddits) {
+    renderDevPage();
 
-  console.log('Fetching all subreddits');
-  fetchAllSubreddits(subreddits);
-
-  console.log('Scheduling Link Refresh');
-  cron.schedule(globals.linkRefreshInterval, () => {
-    console.log('Refreshing links');
+    console.log('Fetching all subreddits');
     fetchAllSubreddits(subreddits);
-  });
-}
 
-function renderDevPage() {
-  renderTemplate(
-    [globals.defaultLink, globals.defaultLink, globals.defaultLink],
-    "dev.html",
-    globals.renderedSubredditsDir,
-    "Dev"
-  )
-  .then(() => console.log("Rendered dev page"))
-}
+    console.log('Scheduling Link Refresh');
+    cron.schedule(globals.linkRefreshInterval, () => {
+      console.log('Refreshing links');
+      fetchAllSubreddits(subreddits);
+    });
+  }
+
+  function renderDevPage() {
+    renderTemplate(
+      [globals.defaultLink, globals.defaultLink, globals.defaultLink],
+      "dev.html",
+      globals.renderedSubredditsDir,
+      "Dev"
+    )
+    .then(() => console.log("Rendered dev page"))
+  }
 
 
-module.exports = scheduleLinkRefresh;
+  module.exports = scheduleLinkRefresh;
