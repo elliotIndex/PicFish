debe73f296f9c2b7e0a008311f31eb16a6af1bbf
Elliot Plant <elliotaplant@gmail.com>
2017-01-13 00:26:36 -0800

    Remove https from picrish

--- server/static/picfish.js
+++ server/static/picfish.js
@@ -1,6 +1,6 @@
 var fbShareBtnStrs = [
-  '<div class="fb-share-button" data-href="https://www.pic.fish/',
-  '" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.pic.fish%2F',
+  '<div class="fb-share-button" data-href="http://www.pic.fish/',
+  '" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.pic.fish%2F',
   '&amp;src=sdkpreparse">Share</a></div>'
 ];
 
