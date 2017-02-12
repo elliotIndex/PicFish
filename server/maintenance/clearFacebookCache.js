eb48f55c805b82f4e60e932692c2ac15ff3bd946
Elliot Plant <elliotaplant@gmail.com>
2017-02-11 19:07:44 -0800

    Facebook link collecting

--- server/maintenance/clearFacebookCache.js
+++ server/maintenance/clearFacebookCache.js
@@ -3,7 +3,7 @@ const request = require('request');
 module.exports = (terminal) => {
   request.post(
     'https://graph.facebook.com',
-    { id: 'http://www.pic.fish/terminal', scrape: true },
+    { id: 'http://www.pic.fish/' + terminal, scrape: true },
     () => {}
   );
 };
