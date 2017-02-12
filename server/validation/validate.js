285c3a5354a0414bee79b8f4b83d141e6e5f9690
Elliot Plant <elliotaplant@gmail.com>
2017-02-11 22:34:43 -0800

    Test if request is the issue

--- server/validation/validate.js
+++ server/validation/validate.js
@@ -8,24 +8,26 @@ const shuffle = require('knuth-shuffle').knuthShuffle
 const validate = linkOrUri => {
   const link = utils.linkOrUriToLink(linkOrUri);
   return new Promise((resolve, reject) => {
-    request(
-      { uri: link.href, timeout: globals.maxValidationRequestTime },
-      (error, response, body) => {
-        if (
-          !error &&
-          response.statusCode === 200 &&
-          utils.isImageResponse(response) &&
-          response.headers['content-length'] < globals.maxLinkSize &&
-          utils.isNotInvalidImgur(response)
-        ) {
-          link.size = response.headers['content-length'];
-          resolve(link);
-        } else {
-          database.insertInvalidLinkId(link.linkId)
-          .then(() => reject(link));
-        }
-      }
-    );
+    link.size = 1000;
+    resolve(link)
+    // request(
+    //   { uri: link.href, timeout: globals.maxValidationRequestTime },
+    //   (error, response, body) => {
+    //     if (
+    //       !error &&
+    //       response.statusCode === 200 &&
+    //       utils.isImageResponse(response) &&
+    //       response.headers['content-length'] < globals.maxLinkSize &&
+    //       utils.isNotInvalidImgur(response)
+    //     ) {
+    //       link.size = response.headers['content-length'];
+    //       resolve(link);
+    //     } else {
+    //       database.insertInvalidLinkId(link.linkId)
+    //       .then(() => reject(link));
+    //     }
+    //   }
+    // );
   })
 }
 
